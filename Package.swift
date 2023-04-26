a0ac1d4c9f4e39fefc18db01721157caf9c1f7f9
Gabriel Olbrisch <97926924+olbrisch@users.noreply.github.com>
2023-04-26 20:10:59 -0300

    [CHANGE] Downgrade iOS deployment target version to 11.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MarieLib",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "MarieLib",
